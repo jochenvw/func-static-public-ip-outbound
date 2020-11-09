2600be3ac1c9c460efee584fcdf81886a83d2304
Jochen van Wylick <jowylick@microsoft.com>
2020-11-09 16:19:34 +0100

    Updated function js code to send HTTP POST to pipedream every 30 seconds

--- app/CronPost/index.js
+++ app/CronPost/index.js
@@ -1,9 +1,35 @@
+const https = require("https")
+const data = JSON.stringify({ "message": "Hello from Azure Function" })
+
 module.exports = async function (context, myTimer) {
     var timeStamp = new Date().toISOString();
-    
-    if (myTimer.IsPastDue)
-    {
+
+    if (myTimer.IsPastDue) {
         context.log('JavaScript is running late!');
     }
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+
+    const options = {
+        hostname: "[yourendpoint].m.pipedream.net",
+        port: 443,
+        path: "/",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+
+    const req = https.request(options, resp => {
+        let data = ""
+        resp.on("data", chunk => {
+            data += chunk
+        })
+        resp.on("end", () => {
+            console.log(JSON.parse(data))
+        })
+    }).on("error", err => {
+        console.error("[error] " + err.message)
+    })
+    req.write(data)
+    req.end()
+    context.log('JavaScript timer trigger function ran!', timeStamp);
 };
